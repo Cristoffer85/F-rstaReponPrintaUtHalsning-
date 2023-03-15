adcabfd178018a53df682d957a28b997ac04d2df
Cristoffer <119351534+Cristoffer85@users.noreply.github.com>
2023-03-15 11:24:54 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class metodSkriverHalsning {
+    public static void main(String[] args) {
+        SayHello();
+
+
+    }
+
+    private static void SayHello() {
+        System.out.println("Hello world!");
+    }
+}
